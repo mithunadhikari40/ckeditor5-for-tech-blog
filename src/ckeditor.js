eeed3bf30c00f1ffe684880c14d2c094fbe49623
Mithun Adhikari <mithunadhikari40@gmail.com>
2022-05-24 14:37:29 +0545

    Removed title from the inline editor

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,20 +7,20 @@ import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
 import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
-import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
+import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js';
+import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
 import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import FindAndReplace from '@ckeditor/ckeditor5-find-and-replace/src/findandreplace.js';
 import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
 import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
 import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
-import HtmlComment from '@ckeditor/ckeditor5-html-support/src/htmlcomment.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
 import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert.js';
@@ -34,18 +34,20 @@ import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
 import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
 import SpecialCharactersCurrency from '@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency.js';
 import SpecialCharactersMathematical from '@ckeditor/ckeditor5-special-characters/src/specialcharactersmathematical.js';
+import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
 import TableCaption from '@ckeditor/ckeditor5-table/src/tablecaption.js';
-import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextPartLanguage from '@ckeditor/ckeditor5-language/src/textpartlanguage.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
-import Title from '@ckeditor/ckeditor5-heading/src/title.js';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
 
 class Editor extends ClassicEditor {}
@@ -56,20 +58,20 @@ Editor.builtinPlugins = [
 	Autoformat,
 	AutoImage,
 	AutoLink,
-	Base64UploadAdapter,
 	BlockQuote,
 	Bold,
-	Code,
+	CKFinderUploadAdapter,
+	CloudServices,
 	CodeBlock,
 	Essentials,
 	FindAndReplace,
 	FontBackgroundColor,
 	FontColor,
+	FontFamily,
 	FontSize,
 	Heading,
 	Highlight,
 	HorizontalLine,
-	HtmlComment,
 	Image,
 	ImageCaption,
 	ImageInsert,
@@ -83,18 +85,20 @@ Editor.builtinPlugins = [
 	Link,
 	List,
 	MediaEmbed,
+	Mention,
 	PageBreak,
 	Paragraph,
 	PasteFromOffice,
 	SpecialCharacters,
 	SpecialCharactersCurrency,
 	SpecialCharactersMathematical,
+	SpecialCharactersText,
+	Strikethrough,
 	Table,
 	TableCaption,
-	TableProperties,
 	TableToolbar,
+	TextPartLanguage,
 	TextTransformation,
-	Title,
 	WordCount
 ];
 
@@ -119,19 +123,23 @@ Editor.defaultConfig = {
 			'mediaEmbed',
 			'undo',
 			'redo',
+			'findAndReplace',
 			'alignment',
-			'code',
 			'codeBlock',
+			'imageInsert',
+			'horizontalLine',
+			'pageBreak',
+			'specialCharacters',
+			'strikethrough',
+			'textPartLanguage',
+			'-',
 			'fontBackgroundColor',
 			'fontColor',
-			'findAndReplace',
-			'fontSize',
+			'fontFamily',
 			'highlight',
-			'horizontalLine',
-			'imageInsert',
-			'specialCharacters',
-			'pageBreak'
-		]
+			'fontSize'
+		],
+		shouldNotGroupWhenFull: true
 	},
 	language: 'en',
 	image: {
@@ -146,8 +154,7 @@ Editor.defaultConfig = {
 		contentToolbar: [
 			'tableColumn',
 			'tableRow',
-			'mergeTableCells',
-			'tableProperties'
+			'mergeTableCells'
 		]
 	}
 };
